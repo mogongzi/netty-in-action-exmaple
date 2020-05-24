6cc669fe6fee6f570a827f74e4cec5ffe53e4013
mogongzi <superrenyuan@hotmail.com>
2020-05-24 20:15:29 +0800

    add README and chapter 1 example

---
+++ chapter1/src/main/java/me/ryan/netty/example/BlockingIOExample.java
@@ -0,0 +1,31 @@
+package me.ryan.netty.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class BlockingIOExample {
+
+    public void serve(int portNumber) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(portNumber);
+        Socket clientSocket = serverSocket.accept();
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        String request, response;
+        while ((request = in.readLine()) != null) {
+            if ("Done".equalsIgnoreCase(request)) {
+                break;
+            }
+
+            response = processRequest(request);
+            out.println(response);
+        }
+    }
+
+    private String processRequest(String request) {
+        return "Processed";
+    }
+}
