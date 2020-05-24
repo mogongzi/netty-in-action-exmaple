28a01114ebb3c3de73f8ed428b616210a8aa30c5
mogongzi <superrenyuan@hotmail.com>
2020-05-24 19:27:36 +0800

    chapter 2 example finished

---
+++ chapter2/echo-server/src/main/java/me/netty/netty/example/EchoServer.java
@@ -0,0 +1,56 @@
+package me.netty.netty.example;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+public class EchoServer {
+
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        if (args.length != 1) {
+            System.err.println("Usage: " + EchoServer.class.getSimpleName() + " <port>");
+        }
+
+        int port = Integer.parseInt(args[0]);
+        new EchoServer(port).start();
+    }
+
+    private void start() throws InterruptedException {
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+
+        bootstrap.group(group)
+                .channel(NioServerSocketChannel.class)
+                .localAddress(new InetSocketAddress(port))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    protected void initChannel(SocketChannel socketChannel) {
+                        socketChannel.pipeline().addLast(serverHandler);
+                    }
+                });
+
+        ChannelFuture future = null;
+        try {
+            future = bootstrap.bind().sync();
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+
+    }
+}
