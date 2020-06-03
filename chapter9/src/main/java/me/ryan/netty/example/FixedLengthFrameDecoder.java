be4f90d1dfe9b4df55fb8c643f07cff6123a61c9
mogongzi <superrenyuan@hotmail.com>
2020-06-03 21:48:59 +0800

    chapter9 inbound embededchannel unit test

---
+++ chapter9/src/main/java/me/ryan/netty/example/FixedLengthFrameDecoder.java
@@ -0,0 +1,28 @@
+package me.ryan.netty.example;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+public class FixedLengthFrameDecoder extends ByteToMessageDecoder {
+
+    private final int frameLength;
+
+    public FixedLengthFrameDecoder(int frameLength) {
+        if (frameLength <= 0) {
+            throw new IllegalArgumentException("frameLength must be positive integer: " + frameLength);
+        }
+        this.frameLength = frameLength;
+    }
+
+
+    @Override
+    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+        while (byteBuf.readableBytes() >= frameLength) {
+            ByteBuf buf = byteBuf.readBytes(frameLength);
+            list.add(buf);
+        }
+    }
+}
---
+++ chapter9/src/test/java/me/ryan/netty/example/FixedLengthFrameDecoderTest.java
@@ -0,0 +1,66 @@
+package me.ryan.netty.example;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.embedded.EmbeddedChannel;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FixedLengthFrameDecoderTest {
+    @Test
+    public void testFramesDecoded() {
+        ByteBuf buf = Unpooled.buffer();
+        for (int i = 0; i < 9; i++) {
+            buf.writeByte(i);
+        }
+
+        ByteBuf input = buf.duplicate();
+        EmbeddedChannel channel = new EmbeddedChannel(new FixedLengthFrameDecoder(3));
+        assertTrue(channel.writeInbound(input.retain()));
+        assertTrue(channel.finish());
+
+        ByteBuf read = channel.readInbound();
+        assertEquals(buf.readSlice(3), read);
+        read.release();
+
+        read = channel.readInbound();
+        assertEquals(buf.readSlice(3), read);
+
+        read = channel.readInbound();
+        assertEquals(buf.readSlice(3), read);
+
+        assertNull(channel.readInbound());
+
+        buf.release();
+    }
+
+    @Test
+    public void testFramesDecoded2() {
+        ByteBuf buf = Unpooled.buffer();
+        for (int i = 0; i < 9; i++) {
+            buf.writeByte(i);
+        }
+        ByteBuf input = buf.duplicate();
+
+        EmbeddedChannel channel = new EmbeddedChannel(new FixedLengthFrameDecoder(3));
+        assertFalse(channel.writeInbound(input.readBytes(2)));
+        assertTrue(channel.writeInbound(input.readBytes(7)));
+
+        assertTrue(channel.finish());
+
+        ByteBuf read = channel.readInbound();
+        assertEquals(buf.readSlice(3), read);
+        read.release();
+
+        read = channel.readInbound();
+        assertEquals(buf.readSlice(3), read);
+
+        read = channel.readInbound();
+        assertEquals(buf.readSlice(3), read);
+
+        assertNull(channel.readInbound());
+
+        buf.release();
+    }
+}
