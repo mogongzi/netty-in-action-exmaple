ee6289e7065b58b31eef450276f0db284c095f4a
mogongzi <superrenyuan@hotmail.com>
2020-06-04 18:19:07 +0800

    chapter10 example

---
+++ chapter10/src/main/java/me/ryan/netty/example/IntegerToStringDecoder.java
@@ -0,0 +1,13 @@
+package me.ryan.netty.example;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageDecoder;
+
+import java.util.List;
+
+public class IntegerToStringDecoder extends MessageToMessageDecoder<Integer> {
+    @Override
+    protected void decode(ChannelHandlerContext channelHandlerContext, Integer integer, List<Object> list) throws Exception {
+        list.add(String.valueOf(integer));
+    }
+}
---
+++ chapter10/src/main/java/me/ryan/netty/example/ToIntegerDecoder2.java
@@ -0,0 +1,14 @@
+package me.ryan.netty.example;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ReplayingDecoder;
+
+import java.util.List;
+
+public class ToIntegerDecoder2 extends ReplayingDecoder<Void> {
+    @Override
+    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+        list.add(byteBuf.readInt());
+    }
+}
