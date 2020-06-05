8955e3d257f855ec089b50940d73d52177fcb400
mogongzi <superrenyuan@hotmail.com>
2020-06-05 14:25:36 +0800

    add chapter11 ssl handler for http protocal example

---
+++ chapter11/src/main/java/me/ryan/netty/example/HttpAggregatorInitializer.java
@@ -0,0 +1,29 @@
+package me.ryan.netty.example;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+public class HttpAggregatorInitializer extends ChannelInitializer<Channel> {
+
+    private final boolean isClient;
+
+    public HttpAggregatorInitializer(boolean isClient) {
+        this.isClient = isClient;
+    }
+
+    @Override
+    protected void initChannel(Channel channel) throws Exception {
+        ChannelPipeline pipeline = channel.pipeline();
+        if (isClient) {
+            pipeline.addLast("codec", new HttpClientCodec());
+        } else {
+            pipeline.addLast("codec", new HttpServerCodec());
+        }
+
+        pipeline.addLast("aggregator", new HttpObjectAggregator(512 * 1024));
+    }
+}
---
+++ chapter11/src/main/java/me/ryan/netty/example/HttpPipelineInitializer.java
@@ -0,0 +1,30 @@
+package me.ryan.netty.example;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpRequestEncoder;
+import io.netty.handler.codec.http.HttpResponseDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+
+public class HttpPipelineInitializer extends ChannelInitializer<Channel> {
+
+    private final boolean client;
+
+    public HttpPipelineInitializer(boolean client) {
+        this.client = client;
+    }
+
+    @Override
+    protected void initChannel(Channel channel) throws Exception {
+        ChannelPipeline pipeline = channel.pipeline();
+        if (client) {
+            pipeline.addLast("decoder", new HttpResponseDecoder());
+            pipeline.addLast("encoder", new HttpRequestEncoder());
+        } else {
+            pipeline.addLast("decoder", new HttpRequestDecoder());
+            pipeline.addLast("encoder", new HttpResponseEncoder());
+        }
+    }
+}
